6997f52501f27148703c6b4d0d02f718f8392cac
talha zulfiqar <csharpjava0@gmail.com>
2024-03-25 23:37:32 +0500

    Update function to handle edge cases

---
+++ src/api/user/user.test.ts
@@ -0,0 +1,21 @@
+import request from 'supertest';
+
+import app from '../../app';
+
+describe('GET /api/v1/users', () => {
+  it('responds with an array of users', async () => {
+    request(app)
+      .get('/api/v1/users')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toHaveProperty('length');
+        expect(response.body.length).toBe(1);
+        expect(response.body[0]).toHaveProperty('id');
+        expect(response.body[0]).toHaveProperty('name');
+        expect(response.body[0]).toHaveProperty('email');
+        expect(response.body[0]).toHaveProperty('password');
+      });
+  });
+});
